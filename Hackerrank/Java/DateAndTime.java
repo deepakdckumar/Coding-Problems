a5a04e47f91cc793ff778b6e1b349ca36521d9b3
Deepak Kumar <deepakdckumar1@gmail.com>
2020-11-05 00:31:34 +0530

    Update DateAndTime.java

--- Hackerrank/Java/DateAndTime.java
+++ Hackerrank/Java/DateAndTime.java
@@ -13,37 +13,37 @@ public class DateAndTime {
 	}
 	
 	public static String findDay(int month, int date, int year) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, year);
-        month -=1;
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.DAY_OF_MONTH, date);
-        String time = calendar.getTime().toString();
-        String day = null;
-        if(time.contains("Sun")) {
-            day = "SUNDAY";
-        }
-        else if(time.contains("Mon")) {
-            day = "MONDAY";
-        }
-        else if(time.contains("Tue")) {
-            day = "TUESDAY";
-        }
-        else if(time.contains("Wed")) {
-            day = "WEDNESDAY";
-        }
-        else if(time.contains("Thu")) {
-            day = "THURSDAY";
-        }
-        else if(time.contains("Fri")) {
-            day = "FRIDAY";
-        }
-        else if(time.contains("Sat")) {
-            day = "SATURDAY";
-        }
-        else {
-            System.out.println("Enter correct date");
-        }
-        return day;
-    }
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, year);
+		month -=1;
+		calendar.set(Calendar.MONTH, month);
+		calendar.set(Calendar.DAY_OF_MONTH, date);
+		String time = calendar.getTime().toString();
+		String day = null;
+		if(time.contains("Sun")) {
+		    day = "SUNDAY";
+		}
+		else if(time.contains("Mon")) {
+		    day = "MONDAY";
+		}
+		else if(time.contains("Tue")) {
+		    day = "TUESDAY";
+		}
+		else if(time.contains("Wed")) {
+		    day = "WEDNESDAY";
+		}
+		else if(time.contains("Thu")) {
+		    day = "THURSDAY";
+		}
+		else if(time.contains("Fri")) {
+		    day = "FRIDAY";
+		}
+		else if(time.contains("Sat")) {
+		    day = "SATURDAY";
+		}
+		else {
+		    System.out.println("Enter correct date");
+		}
+		return day;
+    	}
 }
